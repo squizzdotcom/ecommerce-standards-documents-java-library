ee5e37145d0077dbabf5d6a54bacc5074f2e660d
Rowan Drew <president@squizzdev02.dev.squizz.com.au>
2017-03-13 02:22:25 +1100

    Fixes #4
    Added ESDocumentType and ESDRecordType classes to define the document and record types as static variables.

---
+++ src/org/esd/EcommerceStandardsDocuments/ESDRecordType.java
@@ -0,0 +1,74 @@
+/**
+* Copyright (C) 2017 Squizz PTY LTD
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+package org.esd.EcommerceStandardsDocuments;
+
+/**
+ * Contains the types of Ecommerce Standards Document (ESD) records that the library contains
+ */
+public class ESDRecordType {
+    public static final int ALTERNATE_CODE = 1;
+    public static final int ATTACHMENT = 2;
+    public static final int ATTRIBUTE = 3;
+    public static final int ATTRIBUTE_PROFILE = 4;
+    public static final int ATTRIBUTE_VALUE = 5;
+    public static final int CATEGORY = 6;
+    public static final int COMBINATION_PROFILE = 7;
+    public static final int COMBINATION_PROFILE_FIELD = 8;
+    public static final int CONTACT = 9;
+    public static final int CUSTOMER_ACCOUNT = 10;
+    public static final int CUSTOMER_ACCOUNT_ADDRESS = 11;
+    public static final int CUSTOMER_ACCOUNT_CONTRACT = 12;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_BACKORDER = 13;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_BACKORDER_LINE = 14;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_CREDIT = 15;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_CREDIT_LINE = 16;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_INVOICE = 17;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_INVOICE_LINE = 18;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_ORDER_SALE = 19;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_ORDER_SALE_LINE = 20;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_PAYMENT = 21;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_PAYMENT_LINE = 22;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_PRODUCT_PRICE = 23;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_TRANSACTION = 24;
+    public static final int CUSTOMER_ACCOUNT_PAYMENT = 25;
+    public static final int CUSTOMER_ACCOUNT_PAYMENT_RECORD = 26;
+    public static final int DOWNLOAD = 27;
+    public static final int FLAG = 28;
+    public static final int FLAG_MAPPING = 29;
+    public static final int IMAGE = 30;
+    public static final int ITEM_GROUP = 31;
+    public static final int ITEM_RELATION = 32;
+    public static final int KIT_COMPONENT = 33;
+    public static final int LABOUR = 34;
+    public static final int LOCATION = 35;
+    public static final int ORDER_LINE_ATTRIBUTE = 36;
+    public static final int ORDER_LINE_ATTRIBUTE_PROFILE = 37;
+    public static final int ORDER_LINE_TAX = 38 ;
+    public static final int ORDER_PAYMENT = 39;
+    public static final int ORDER_PRODUCT_DELIVERY = 40;
+    public static final int ORDER_PURCHASE = 41;
+    public static final int ORDER_PURCHASE_LINE = 42;
+    public static final int ORDER_SALE = 43;
+    public static final int ORDER_SALE_LINE = 44;
+    public static final int ORDER_SURCHARGE = 45;
+    public static final int PAYMENT_TYPE = 46;
+    public static final int PRICE = 47;
+    public static final int PRICE_LEVEL = 48;
+    public static final int PRODUCT = 49;
+    public static final int PRODUCT_COMBINATION = 50;
+    public static final int PRODUCT_COMBINATION_PARENT = 51;
+    public static final int PURCHASER = 52;
+    public static final int SALES_REP = 53;
+    public static final int SELL_UNIT = 54;
+    public static final int STOCK_QUANTITY = 55;
+    public static final int SUPPLIER_ACCOUNT = 56;
+    public static final int SUPPLIER_ACCOUNT_ADDRESS = 57;
+    public static final int SUPPLIER_ACCOUNT_ENQUIRY_ORDER_PURCHASE = 58;
+    public static final int SUPPLIER_ACCOUNT_ENQUIRY_ORDER_PURCHASE_LINE = 59;
+    public static final int SURCHARGE = 60;
+    public static final int TAXCODE = 61;
+}
---
+++ src/org/esd/EcommerceStandardsDocuments/ESDocumentType.java
@@ -0,0 +1,49 @@
+/**
+* Copyright (C) 2017 Squizz PTY LTD
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+package org.esd.EcommerceStandardsDocuments;
+
+/**
+ * Contains the types of Ecommerce Standards Documents (ESD) that the library contains
+ */
+public class ESDocumentType 
+{
+    public static final int ALTERNATE_CODE = 9;
+    public static final int ATTACHMENT = 13;
+    public static final int ATTRIBUTE = 11;
+    public static final int CATEGORY = 8;
+    public static final int CUSTOMER_ACCOUNT = 17;
+    public static final int CUSTOMER_ACCOUNT_ADDRESS = 20;
+    public static final int CUSTOMER_ACCOUNT_CONTRACT = 19;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY = 29;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_LINE = 30;
+    public static final int CUSTOMER_ACCOUNT_ENQUIRY_PRODUCT_PRICE = 31;
+    public static final int CUSTOMER_ACCOUNT_PAYMENT =32;
+    public static final int DOWNLOAD = 33;
+    public static final int FLAG = 21;
+    public static final int IMAGE = 12;
+    public static final int ITEM_GROUP = 34;
+    public static final int ITEM_RELATION = 14;
+    public static final int KIT = 22;
+    public static final int LABOUR = 35;
+    public static final int LOCATION = 23;
+    public static final int ORDER_PURCHASE = 24;
+    public static final int ORDER_SALE = 36;
+    public static final int PAYMENT_TYPE = 27;
+    public static final int PRICE = 37;
+    public static final int PRICE_LEVEL = 2;
+    public static final int PRODUCT = 3;
+    public static final int PRODUCT_COMBINATION = 15;
+    public static final int PURCHASER = 25;
+    public static final int SALES_REP = 16;
+    public static final int SELL_UNIT = 28;
+    public static final int STOCK_QUANTITY = 10;
+    public static final int SUPPLIER_ACCOUNT = 18;
+    public static final int SUPPLIER_ACCOUNT_ADDRESS = 38;
+    public static final int SUPPLIER_ACCOUNT_ENQUIRY = 39;
+    public static final int SURCHARGE = 26;
+    public static final int TAXCODE = 1;
+}
